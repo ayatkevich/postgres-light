c9d15e79ea37cd41f683fd6e1314367f562d9e17
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-07 00:17:24 +0700

    make it work as a fragment

--- src/spec.ts
+++ src/spec.ts
@@ -11,4 +11,13 @@ describe("postgres-lite", () => {
   it("should be awaitable", async () => {
     expect(await postgresSql`select 1`).toEqual(await postgresLiteSql`select 1`);
   });
+
+  it("should work as a fragment", async () => {
+    await _(postgresSql);
+    await _(postgresLiteSql as typeof postgresSql);
+    async function _(sql: typeof postgresSql) {
+      const fragment = sql`select 1`;
+      expect(await sql`select (${fragment})`).toEqual([{ "?column?": 1 }]);
+    }
+  });
 });
